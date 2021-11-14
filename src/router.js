0d3f8b774bbd8a77aa1d8ec608529f485d665b1d
wahyu.alfarisi-os <wahyu.alfarisi-os@contractor.bukalapak.com>
2021-11-14 17:20:43 +0700

    define routes

--- src/router.js
+++ src/router.js
@@ -1,16 +1,22 @@
 import { createRouter, createWebHistory } from 'vue-router';
+import CoachesLists from './pages/coaches/CoachesLists.vue';
+import CoachDetails from './pages/coaches/CoachDetails.vue';
+import CoachRegister from './pages/coaches/CoachRegisters.vue';
+import ContactCoach from './pages/requests/ContactCoach.vue';
+import RequestReceived from './pages/requests/RequestReceived.vue';;
+import NotFound from './pages/NotFound.vue';
 
 const router = createRouter({
     history: createWebHistory(),
     routes: [
         { path: '/', redirect: '/coaches'},
-        { path: '/coaches', component: null },
-        { path: '/coaches/:id', component: null, children: [
-            { path: 'contact', component: null }
+        { path: '/coaches', component: CoachesLists },
+        { path: '/coaches/:id', component: CoachDetails, children: [
+            { path: 'contact', component: ContactCoach }
         ] },
-        { path: '/register', component: null },
-        { path: '/requests', component: null },
-        { path: '/:notFound()', component: null }
+        { path: '/register', component: CoachRegister },
+        { path: '/requests', component: RequestReceived },
+        { path: '/:notFound()', component: NotFound }
     ]
 })
 
